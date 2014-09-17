a1ce0449b3b70ec8ab566a563f551cbd590bba92
Simone Soldateschi <simone.soldateschi@gmail.com>
2014-09-17 22:00:46 +0100

    Added coloured messages

--- pybaseproject/foo.py
+++ pybaseproject/foo.py
@@ -11,6 +11,10 @@ def main():
     bar = pybaseproject.bar.Bar()
     print bar.echo('Hola!')
     logger.debug('debug message')
+    logger.info('info message')
+    logger.warning('warning message')
+    logger.error('error message')
+    logger.critical('critical message')
 
 if __name__ == '__main__':
     main()
